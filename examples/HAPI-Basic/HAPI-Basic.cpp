252f87fb1a9b218a2204d42a0b7ac50b8c003383
Ivan Golubic <ivan@mvt-solutions.com>
2020-03-30 16:15:05 +0400

    Updated example

--- examples/HAPI-Basic/HAPI-Basic.cpp
+++ examples/HAPI-Basic/HAPI-Basic.cpp
@@ -13,7 +13,7 @@ void setup()
 
     Serial.begin(115200);
 
-    iotaap.wifi.connect("SSID", "PASSWORD", true);
+    iotaap.wifi.connect("SSID", "PASSWORD");
     hapi.configure("DEVICE_ID", "DEVICE_TOKEN", "MQTT_SERVER", "MQTT_USERNAME", "MQTT_PASSWORD", callback);
 }
 
